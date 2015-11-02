7da2e1020e89d1495bb91b781c08d4f6cc055c87
Tim Wade <hello@timjwade.com>
2015-11-01 21:09:49 -0800

    First successful feature: user creation

--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -3,11 +3,10 @@ require "rails_helper"
 RSpec.feature "User Sign up" do
   scenario "User creates a new account" do
     visit "/"
-    fill_in "Name", with: "user@example.com"
+    fill_in "Email", with: "user@example.com"
     fill_in "Password", with: "abc123"
     click_button "Create Account"
 
     expect(page).to have_text("Account was successfully created")
   end
-
 end
