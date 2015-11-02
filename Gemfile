dba72c539ea880a4e55aa13c039a005cc256244e
Tim Wade <hello@timjwade.com>
2015-11-01 21:19:11 -0800

    Add clearance to handle authentication

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ gem "jquery-rails"
 gem "jbuilder", "~> 2.0"
 gem "sdoc", "~> 0.4.0", group: :doc
 
+gem "clearance"
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102045142) do
+ActiveRecord::Schema.define(version: 20151102051542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20151102045142) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password"
+    t.string "encrypted_password", limit: 128
+    t.string "confirmation_token", limit: 128
+    t.string "remember_token",     limit: 128
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", using: :btree
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
 end
