2d978e8941d344bec0c6f7eb44ac7a22262f88d2
Tim Wade <hello@timjwade.com>
2015-11-13 21:28:42 -0800

    Extract `current_chart`

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -2,11 +2,11 @@ class SubmissionsController < ApplicationController
   before_action :require_login
 
   def new
-    @chart = current_user.chart
+    @chart = current_chart
   end
 
   def create
-    current_user.chart.submissions.create(data: submission_params)
+    current_chart.submissions.create(data: submission_params)
   end
 
   private
