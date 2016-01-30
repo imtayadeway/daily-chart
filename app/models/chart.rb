0b280928ae7d183d754f5e365cd0500ab8285ee6
Tim Wade <hello@timjwade.com>
2016-01-30 13:02:36 -0800

    Push stuff down into chart

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -32,4 +32,40 @@ class Chart < ActiveRecord::Base
   def percentages
     scorables.map(&:percent)
   end
+
+  def last_seven_days
+    last(7).map(&:weekday)
+  end
+
+  def daily_percentages
+    last(7).map(&:percent)
+  end
+
+  def weekly_averages
+    scorables.each_slice(7).map do |week|
+      week.map(&:percent).inject(:+) / week.size
+    end
+  end
+
+  def weeks_all_time
+    1.upto((scorables.size / 7) + 1).to_a
+  end
+
+  def best_this_week
+    items.max_by { |item| weekly_score_for(item) }.name
+  end
+
+  def worst_this_week
+    items.min_by { |item| weekly_score_for(item) }.name
+  end
+
+  private
+
+  def last(x_days)
+    scorables.last(x_days)
+  end
+
+  def weekly_score_for(item)
+    last(7).map { |scorable| scorable.score_for(item.name) }.reduce(:+)
+  end
 end
--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -1,44 +1,10 @@
 class Dashboard
   attr_reader :chart
-  delegate :submission_pending?, :scorables, to: :chart
+  delegate :weekly_averages, :weeks_all_time, :best_this_week, :worst_this_week,
+           :last_seven_days, :daily_percentages, :submission_pending?,
+           :scorables, to: :chart
 
   def initialize(chart)
     @chart = chart
-  end
-
-  def last_seven_days
-    last(7).map(&:weekday)
-  end
-
-  def daily_percentages
-    last(7).map(&:percent)
-  end
-
-  def weekly_averages
-    scorables.each_slice(7).map do |week|
-      week.map(&:percent).inject(:+) / week.size
-    end
-  end
-
-  def weeks_all_time
-    1.upto((scorables.size / 7) + 1).to_a
-  end
-
-  def best_this_week
-    chart.items.max_by { |item| weekly_score_for(item) }.name
-  end
-
-  def worst_this_week
-    chart.items.min_by { |item| weekly_score_for(item) }.name
-  end
-
-  private
-
-  def last(x_days)
-    scorables.last(x_days)
-  end
-
-  def weekly_score_for(item)
-    last(7).map { |scorable| scorable.score_for(item.name) }.reduce(:+)
   end
 end
