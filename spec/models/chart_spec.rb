6e9043916bdccb244c5648e32bc7cdffafe5da8a
Tim Wade <hello@timjwade.com>
2016-01-31 10:05:10 -0800

    Add some tests for Chart#last_seven_days

--- spec/models/chart_spec.rb
+++ spec/models/chart_spec.rb
@@ -62,4 +62,24 @@ RSpec.describe Chart do
       expect(chart.percentages).to eq([100, 100])
     end
   end
+
+  describe "#last_seven_days" do
+    it "counts today if it's been submitted" do
+      chart = Chart.create(items: [Item.new(name: "foo")])
+      6.downto(0) do |n|
+        Timecop.freeze(Time.zone.today - n) { chart.submissions.create(data: { "foo" => "1" }) }
+      end
+
+      expect(chart.last_seven_days.last).to eq(Time.zone.today.strftime("%a"))
+    end
+
+    it "does not count today if a submission is pending" do
+      chart = Chart.create(items: [Item.new(name: "foo")])
+      7.downto(1) do |n|
+        Timecop.freeze(Time.zone.today - n) { chart.submissions.create(data: { "foo" => "1" }) }
+      end
+
+      expect(chart.last_seven_days.last).to eq((Time.zone.yesterday).strftime("%a"))
+    end
+  end
 end
