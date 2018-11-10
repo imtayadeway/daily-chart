5de0dc7e4632c45d5d0ae00247ab2bc1ecdea174
Tim Wade <hello@timjwade.com>
2018-11-10 12:46:48 -0800

    Test Dashboard#last_seven_days
    This used to be on chart, so these tests were skipped. Moving them to
    the dashboard specs and updating allows us to un-pend them!

--- spec/models/chart_spec.rb
+++ spec/models/chart_spec.rb
@@ -9,23 +9,4 @@
 require "rails_helper"
 
 RSpec.describe Chart do
-  xdescribe "#last_seven_days" do
-    it "counts today if it's been submitted" do
-      chart = Chart.create(items: [Item.new(name: "foo")])
-      6.downto(0) do |n|
-        chart.submissions.create(data: { "foo" => "1" }, date: Time.zone.today - n)
-      end
-
-      expect(chart.last_seven_days.last).to eq(Time.zone.today.strftime("%a"))
-    end
-
-    it "does not count today if a submission is pending" do
-      chart = Chart.create(items: [Item.new(name: "foo")])
-      7.downto(1) do |n|
-        chart.submissions.create(data: { "foo" => "1" }, date: Time.zone.today - n)
-      end
-
-      expect(chart.last_seven_days.last).to eq((Time.zone.yesterday).strftime("%a"))
-    end
-  end
 end
