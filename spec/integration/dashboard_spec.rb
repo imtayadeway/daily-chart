0d214ad0281f659dc02176554895280b9e2376db
Tim Wade <hello@timjwade.com>
2018-11-10 12:44:13 -0800

    Convert controller specs to integration specs

--- spec/integration/dashboard_spec.rb
+++ spec/integration/dashboard_spec.rb
@@ -1,22 +1,21 @@
 require "rails_helper"
 
-RSpec.describe DashboardsController, type: :controller do
+RSpec.describe "Dashboards", type: :request do
   it "takes me to the sign up page when not logged in" do
-    get :show
+    get dashboard_url
     expect(response).to redirect_to(sign_in_path)
   end
 
   it "loads the dashboard if logged in with a chart" do
     user = create(:user)
     user.create_chart(items_attributes: [{ name: "Exercise" }])
-    sign_in_as(user)
-    get :show
+    get dashboard_url, params: { as: user.id }
     expect(response).to have_http_status(:ok)
   end
 
   it "takes me to the new chart page if logged in without a chart" do
-    sign_in
-    get :show
+    user = create(:user)
+    get dashboard_url, params: { as: user.id }
     expect(response).to redirect_to(new_chart_path)
   end
 end
--- spec/integration/welcome_spec.rb
+++ spec/integration/welcome_spec.rb
@@ -1,22 +1,21 @@
 require "rails_helper"
 
-RSpec.describe WelcomeController, type: :controller do
+RSpec.describe "Welcome", type: :request do
   it "takes me to the sign up page" do
-    get :index
+    get root_url
     expect(response).to redirect_to(sign_up_path)
   end
 
   it "takes me to the dashboard if logged in with a chart" do
     user = create(:user)
     user.create_chart(items_attributes: [{ name: "Exercise" }])
-    sign_in_as(user)
-    get :index
+    get root_url, params: { as: user.id }
     expect(response).to redirect_to(dashboard_path)
   end
 
   it "takes me to the new chart page if logged in without a chart" do
-    sign_in
-    get :index
+    user = create(:user)
+    get root_url, params: { as: user.id }
     expect(response).to redirect_to(new_chart_path)
   end
 end
