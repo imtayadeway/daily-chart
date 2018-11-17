284ae59d866c87b9e30ad65b0347b0c5441c12bd
Tim Wade <hello@timjwade.com>
2018-11-17 10:13:52 -0800

    Drop submissions.data column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_14_034047) do
+ActiveRecord::Schema.define(version: 2018_11_17_041441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 2018_11_14_034047) do
 
   create_table "submissions", id: :serial, force: :cascade do |t|
     t.integer "score"
-    t.json "data"
     t.integer "chart_id"
     t.date "date"
   end
